9ef6aad7657ab2f342bf14f3ece9668a01e37cb9
afer <791143125@qq.com>
2015-07-20 09:43:22 +0800

    Initial Commit

---
+++ QdaiV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QdaiV
+//
+//  Created by 口贷网 on 15/7/20.
+//  Copyright (c) 2015年 Afer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
